d480a6f1fce52d14e22e7547712cca8610452f35
turupawn <ahmed.hn.43@gmail.com>
2021-05-30 21:03:57 -0600

    now using erc777

--- contracts/MyERC20.sol
+++ contracts/MyERC20.sol
@@ -1,23 +1,22 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.0;
 
-import "./dependencies/ERC20.sol";
+import "./dependencies/ERC777.sol";
 
-contract MyERC20 is ERC20 {
-
-  uint256 public numero = 10;
+contract MyERC20 is ERC777 {
 
   constructor () public
-    ERC20("testground1", "TG1")
+    ERC777("testground2", "TG2", new address[](0))
   {
-    _mint(msg.sender, 101 * 10 ** uint(decimals()));
+    uint256 initialSupply = 1001 ether;
+    _mint(msg.sender, initialSupply, "", "");
   }
 
   function burn(uint256 amount) public {
-      _burn(msg.sender, amount);
+    _burn(msg.sender, amount, "", "");
   }
 
   function mint(uint256 amount) public {
-    _mint(msg.sender, amount);
+    _mint(msg.sender, amount, "", "");
   }
 }
